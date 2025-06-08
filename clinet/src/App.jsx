0d210b550085a500f050ffde8c38ca95fc589a6c
therajat14 <heylegend14@gmail.com>
2025-06-08 22:29:31 +0530

    feat: Create frontend (client) application file structure

---
+++ clinet/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 class="bg-sky-700 p-0.5 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+        Hey MF do your Work now
+      </h1>
+    </>
+  );
+}
+
+export default App;
