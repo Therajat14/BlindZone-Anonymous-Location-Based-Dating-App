9ef1f9869ebe8844a10cb6130af36f60d0781903
therajat14 <heylegend14@gmail.com>
2025-06-10 03:51:57 +0530

    Proper Comments in hash util

--- server/utils/hash.js
+++ server/utils/hash.js
@@ -1,13 +1,17 @@
 const bcrypt = require("bcrypt");
 
+// Set salt rounds (default to 10 if not set in .env)
 const SALT_ROUNDS = parseInt(process.env.SALT_ROUNDS) || 10;
 
+// Hash a password using bcrypt
 const generateHash = async (password) => {
   return await bcrypt.hash(password, SALT_ROUNDS);
 };
 
+// Compare a plain password with a hashed one
 const verifyHash = async (password, hash) => {
   return await bcrypt.compare(password, hash);
 };
 
+// Export the functions
 module.exports = { generateHash, verifyHash };
