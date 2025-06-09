5231465c67b53d33869490a2c99569acdab92979
therajat14 <heylegend14@gmail.com>
2025-06-10 02:30:30 +0530

    Created SignUp Routes with hash and jwt

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,14 +1,37 @@
-const { generateHash, verifyHash } = require("../utils/hash");
+const { generateHash } = require("../utils/hash");
 const User = require("../models/User");
+const { jwtSign } = require("../utils/jwt");
 
 const signUp = async (req, res) => {
-  const { email, password } = req.body;
-  const user = new User({
-    email,
-    password: await generateHash(password),
-  });
+  try {
+    const { email, password } = req.body;
 
-  user.save().then((response) => res.status(201).json(response));
+    // Check if the email already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(403).json({
+        msg: "Email already exists",
+      });
+    }
+
+    // Create new user
+    const hashedPassword = await generateHash(password);
+    const user = new User({ email, password: hashedPassword });
+    const savedUser = await user.save();
+
+    // Generate JWT after saving, optionally include _id
+    const jwtToken = jwtSign({ id: savedUser._id, email: savedUser.email });
+
+    res.status(201).json({
+      msg: "User created successfully",
+      jwtToken,
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({
+      msg: "Internal Server Error",
+    });
+  }
 };
 
 const logIn = (req, res) => {
